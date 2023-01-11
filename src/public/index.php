f770cd3065bd2d65651262079879a095830946fc
Johirul Islam <johirpro@gmail.com>
2023-01-11 10:30:20 +0600

    initial commit

---
+++ src/public/index.php
@@ -0,0 +1,11 @@
+<?php
+$pdo = new PDO('mysql:dbname=example_db;host=mysql', 'dbuser', 'userentry', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$query = $pdo->query('SHOW VARIABLES like "version"');
+
+$row = $query->fetch();
+
+echo 'MySQL version:' . $row['Value'];
+
+
+phpinfo();
